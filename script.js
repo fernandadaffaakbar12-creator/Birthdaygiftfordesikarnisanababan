0ab0871affa8b3783cb0698052ee0e3e7df05d91
Fernanda Daffa <emailkamu@gmail.com>
2026-08-25 15:20:25 +0700

    COMMIT

--- script.js
+++ script.js
@@ -175,7 +175,7 @@
         const pinPopupClose = document.getElementById('pin-popup-close');
 
         // DEFAULT PIN: Silakan ubah angka ini jika ingin PIN lain
-        const SECRET_PIN = "2104";
+        const SECRET_PIN = "0523";
 
         let pinAttempt = 0;
         let popupTimeout = null;
@@ -563,6 +563,7 @@ function ketikTeks(elemen, teks) {
     return new Promise(resolve => {
         let index = 0;
         elemen.innerHTML = '';
+        elemen.classList.add('typing-active');
 
         function ketik() {
             if (index < teks.length) {
@@ -570,6 +571,7 @@ function ketikTeks(elemen, teks) {
                 index++;
                 setTimeout(ketik, 35);
             } else {
+                elemen.classList.remove('typing-active');
                 elemen.classList.add('typing-done');
                 resolve();
             }
